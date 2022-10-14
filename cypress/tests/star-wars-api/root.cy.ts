659a86d051b55987052f012cdaf0259df44ecf0b
dodosaurus <kovac.jozef25@gmail.com>
2022-10-14 10:42:01 +0200

    formatting, adjusting allure script

--- cypress/tests/star-wars-api/root.cy.ts
+++ cypress/tests/star-wars-api/root.cy.ts
@@ -1,17 +1,26 @@
-import spok from 'cy-spok'
+import spok from "cy-spok";
 
-describe('Root', () => {
-    let expectedResourcesInOrder = ['people', 'planets', 'films', 'species', 'vehicles', 'starships']
+describe("Root", () => {
+  let expectedResourcesInOrder = [
+    "people",
+    "planets",
+    "films",
+    "species",
+    "vehicles",
+    "starships",
+  ];
 
-    it('Get all root URLs - happy path', () => {
-        cy.getRoot().then(res => {
-            let keys = Object.keys(res.body)
-            cy.wrap(keys).should(spok(expectedResourcesInOrder))
+  it("Get all URLs - happy path", () => {
+    cy.getRoot().then((res) => {
+      let keys = Object.keys(res.body);
+      cy.wrap(keys).should(spok(expectedResourcesInOrder));
 
-            expectedResourcesInOrder.forEach((key, index) => {
-                let expURL = `${Cypress.config('baseUrl')}/${expectedResourcesInOrder[index]}/`
-                expect(res.body[key]).to.be.eq(expURL)
-            })
-        })
+      expectedResourcesInOrder.forEach((key, index) => {
+        let expURL = `${Cypress.config("baseUrl")}/${
+          expectedResourcesInOrder[index]
+        }/`;
+        expect(res.body[key]).to.be.eq(expURL);
+      });
     });
+  });
 });
