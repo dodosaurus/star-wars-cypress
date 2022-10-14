7af55df0ed29f631a0944e7ae4afc109e252c1f6
dodosaurus <kovac.jozef25@gmail.com>
2022-10-14 09:34:07 +0200

    allure implemented

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,17 +1,20 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
+import allureWriter from "@shelex/cypress-allure-plugin/writer";
 
 module.exports = defineConfig({
   video: false,
   viewportWidth: 1080,
   viewportHeight: 1080,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.ts')(on, config)
+      allureWriter(on, config);
+      return config;
     },
-    baseUrl: 'https://swapi.dev/api',
-    supportFile:'cypress/support/tests.ts',
-    specPattern: 'cypress/tests/**/*.cy.{js,jsx,ts,tsx}',
+    baseUrl: "https://swapi.dev/api",
+    supportFile: "cypress/support/tests.ts",
+    specPattern: "cypress/tests/**/*.cy.{js,jsx,ts,tsx}",
   },
-})
+  env: {
+    allureResultsPath: "allure/allure-results"
+  }
+});
--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -16,7 +16,10 @@
  */
 // eslint-disable-next-line no-unused-vars
 
-module.exports = (on, config) => {
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
+module.exports = (on, config) => {
+    allureWriter(on, config);
+    return config;
 
 }
